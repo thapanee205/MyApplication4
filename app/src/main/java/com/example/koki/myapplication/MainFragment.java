074f86ecb001a9b8099fd4e064d810c12980cb19
koki <thapanee.uki@gmail.com>
2018-11-19 15:33:02 +0700

    Replace Fregment success

--- app/src/main/java/com/example/koki/myapplication/MainFragment.java
+++ app/src/main/java/com/example/koki/myapplication/MainFragment.java
@@ -2,10 +2,12 @@ package com.example.koki.myapplication;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,10 +15,29 @@ import android.view.ViewGroup;
  */
 public class MainFragment extends Fragment {
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    //    Register Controller
+        TextView textView = getView().findViewById(R.id.textViewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+           //     Replace Flagment
+
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.layoutMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+    }  // Main Method
 
     public MainFragment() {
         // Required empty public constructor
-    }
+    }// Constructor เชื่อมต่อข้อมูล
 
 
     @Override
@@ -24,6 +45,6 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
-    }
+    } // Create View
 
-}
+} //main Class
