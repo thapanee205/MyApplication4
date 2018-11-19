63e4347ca8632996eca09a3e2e33f8e523411f44
koki <thapanee.uki@gmail.com>
2018-11-19 16:38:09 +0700

    Finish 19 /11/ 2561

--- app/src/main/java/com/example/koki/myapplication/RegisterFragment.java
+++ app/src/main/java/com/example/koki/myapplication/RegisterFragment.java
@@ -2,7 +2,9 @@ package com.example.koki.myapplication;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,21 @@ import android.view.ViewGroup;
  */
 public class RegisterFragment extends Fragment {
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+     //   Create Toolbar
+          createToolbar();
+
+
+    }//Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toobarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.register);
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(R.string.messege_have_space);
+    }
 
     public RegisterFragment() {
         // Required empty public constructor
