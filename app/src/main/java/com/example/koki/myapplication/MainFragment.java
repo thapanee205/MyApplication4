40f1ac955b05f2d72419281c384a8b7b335ca7eb
koki <thapanee.uki@gmail.com>
2018-11-21 14:03:00 +0700

    Sigh out Success

--- app/src/main/java/com/example/koki/myapplication/MainFragment.java
+++ app/src/main/java/com/example/koki/myapplication/MainFragment.java
@@ -1,6 +1,7 @@
 package com.example.koki.myapplication;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 /**
@@ -18,7 +21,21 @@ public class MainFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-    //    Register Controller
+
+      //  check status
+        FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null ) {
+            startActivity(new Intent(getActivity(), ServiceActivity.class));
+            getActivity().finish();
+        }
+
+
+        //    Register Controller
+        registerController();
+
+    }  // Main Method
+
+    private void registerController() {
         TextView textView = getView().findViewById(R.id.textViewRegister);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,8 +49,7 @@ public class MainFragment extends Fragment {
                         .commit();
             }
         });
-
-    }  // Main Method
+    }
 
     public MainFragment() {
         // Required empty public constructor
--- app/src/main/java/com/example/koki/myapplication/ServiceActivity.java
+++ app/src/main/java/com/example/koki/myapplication/ServiceActivity.java
@@ -1,13 +1,64 @@
 package com.example.koki.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ServiceActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+//        Create toolbar
+
+        createToolbar();
+
+
+    } //main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==R.id.itemExit) {
+            signOutService();
+        }
+
+
+
+
+        return super.onOptionsItemSelected(item);
     }
-}
+
+    private void signOutService() {
+        FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
+        firebaseAuth.signOut();
+        startActivity( new Intent( ServiceActivity.this, MainActivity.class));
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_service, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarService);
+        setSupportActionBar(toolbar);
+        // Show Login User
+        FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+        getSupportActionBar().setSubtitle(firebaseUser.getDisplayName());
+
+
+    }
+}// Main Class
